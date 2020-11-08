9112ec44e46f948709e0091631cd12764d1533fc
afraj97 <67052878+afraj97@users.noreply.github.com>
2020-11-08 17:44:55 +0530

    files added

---
+++ script.js
@@ -0,0 +1,60 @@
+var unit=document.getElementById('tunits');
+
+function resetText()
+{
+	unit=0;
+	console.log('value cleared');
+}
+function billCal()
+{
+	
+	
+	var kwh,fr,amount;
+	if(unit<=58)
+	{
+		
+		if(unit>=30)
+		{
+		    fr =60;
+			kwh=29*2.5+((unit-29)*4.85);
+			amount=kwh+fr;
+		}
+		else
+		{
+			fr=30
+			kwh=unit*2.5;
+			amount=kwh+fr;
+		}
+		
+	}
+	else
+	{
+		if(unit>=175)
+		{
+			fr=540
+			kwh=(58*7.85)+(29*10)+(29*27.75)+(58*32)+((unit-174)*45);
+			amount=kwh+fr;
+		}
+		else if(unit>=117 && unit<=174)
+		{
+			fr=480;
+			kwh=(58*7.85)+(29*10)+(29*27.75)+((unit-116)*32);
+			amount=kwh+fr
+		}
+		else if(unit>=88 && unit<=116)
+		{
+			fr=480;
+			kwh=(58*7.85)+(29*10)+((unit-87)*27.75);
+			amount=kwh+fr;
+		}
+		else
+		{
+			fr=90;
+			kwh=(58*7.85)+((unit-58)*10);
+			amount=kwh+fr;
+		}
+	}
+	let tResult=document.getElementById('replace');
+	tResult.innerHTML="<p>KWH Charge:  Rs "+kwh+"&nbsp &nbsp Fixed rate:  Rs "+fr+"</p>Total: Rs "+amount;
+
+}
